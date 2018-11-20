15b69b4fd1b0d60d4466d9e069274358a68bb1e9
Diana Otel <dianaotel@ECS0832.local>
2018-11-20 12:37:08 +0000

    Removed tests

--- test-auto/cypress/support/selectors.js
+++
@@ -1,5 +0,0 @@
-export const cityInput = 'input[name="city"]'
-
-export const getWeatherButton = 'input[value="Get Weather"]'
-
-export const temperatureResult = 'fieldset p'
--- test-auto/cypress/support/test-data.js
+++
@@ -1,3 +0,0 @@
-export const city = {
-    london: 'London'
-}
